f3f966f60eab2a8c5514ec9fae88321fa81feb3a
r.rader <r.rader@smartweb.com.ua>
2017-06-25 00:31:48 +0300

    Refactor single experiment

--- elastic_test/tests/single_droplet.py
+++ elastic_test/tests/single_droplet.py
@@ -20,19 +20,44 @@ class SourceAndTarget(LayoutedBase):
 
         for droplet in self.get_droplet_group('Target'):
             with self.ssh_droplet(droplet) as ssh:
-                stdin, stdout, stderr = ssh.exec_command('bash /opt/test/assets/scripts/metrics.sh')
-                print(stdout.read().decode(), stderr.read().decode())
+                self.setup(ssh)
 
+        self.before_experiment()
+        self.experiment()
+        self.after_experiment()
+
+        self.collect()
+
+    def experiment(self):
+        sleep(30)
+
+    def before_experiment(self):
+        for droplet in self.get_droplet_group('Target'):
+            with self.ssh_droplet(droplet) as ssh:
                 ssh.exec_command('rm -f /var/log/metrics.sh.log')
                 ssh.exec_command('service metrics.sh start')
                 sleep(30)
                 ssh.exec_command('service metrics.sh stop')
 
+    def after_experiment(self):
+        for droplet in self.get_droplet_group('Target'):
+            with self.ssh_droplet(droplet) as ssh:
+                ssh.exec_command('service metrics.sh stop')
+
+    def collect(self):
+        for droplet in self.get_droplet_group('Target'):
+            with self.ssh_droplet(droplet) as ssh:
                 now = time()
-                output = f'output/{now}/{droplet.name}'
+                output = f'output/{now}/Target/{droplet.name}'
                 os.makedirs(output)
                 sftp = ssh.open_sftp()
                 sftp.get('/var/log/metrics.sh.log', f'{output}/metrics.sh.log')
+
+    def setup(self):
+        for droplet in self.get_droplet_group('Target'):
+            with self.ssh_droplet(droplet) as ssh:
+                stdin, stdout, stderr = ssh.exec_command('bash /opt/test/assets/scripts/metrics.sh')
+                print(stdout.read().decode(), stderr.read().decode())
 
 
 if __name__ == "__main__":
